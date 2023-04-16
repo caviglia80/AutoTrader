ea5f107b6648bf1a809a58fe85686f73559aeabc
Fran <caviglia80@gmail.com>
2023-04-16 13:52:39 -0300

    ..

--- autoTrader/DB.vb
+++ autoTrader/DB.vb
@@ -204,7 +204,7 @@ Module DB
 				SQLiteConnection.Open()
 
 				Dim comment As String = String.Concat("BUY-> BTC: ", CAMBIO24HS_BTC.ToString("0.00"), "%, Sensibilidad: ", SENSIBILIDAD_COMPRA.ToString(), "%, ", Comments)
-				Dim tend As String = String.Concat(Tendencia(Coin.Symbol, "1s", 30), "/", Tendencia(Coin.Symbol, "15m", 4), "/", Tendencia(Coin.Symbol, "30m", 4), "/", Tendencia(Coin.Symbol, "1h", 4))
+				Dim tend As String = String.Concat(Tendencia(Coin.Symbol, "1s", 3), "/", Tendencia(Coin.Symbol, "1s", 5), "/", Tendencia(Coin.Symbol, "1s", 8), "/", Tendencia(Coin.Symbol, "1s", 10), "/", Tendencia(Coin.Symbol, "1s", 15))
 				Using cmd As New SQLiteCommand With {
 					.Connection = SQLiteConnection,
 					.CommandText = String.Concat("INSERT INTO tBuys (Coin, Date, USDT, Quantity, MarketPrice, Comments, Tendencia) VALUES (""", Coin.Symbol, """,""", _Date, """,""", USDT.Replace(",", "."), """,""", Quantity, """,""", Coin.lastPrice.ToString().Replace(",", "."), """,""", comment, """,""", tend, """);")}
@@ -277,7 +277,7 @@ Module DB
 				SQLiteConnection.Open()
 
 				Dim Comments As String = String.Concat("BTC: ", CAMBIO24HS_BTC.ToString("0.00"), "%, ")
-				Dim tend As String = String.Concat(Tendencia(Coin.Symbol, "1s", 30), "/", Tendencia(Coin.Symbol, "15m", 4), "/", Tendencia(Coin.Symbol, "30m", 4), "/", Tendencia(Coin.Symbol, "1h", 4))
+				Dim tend As String = String.Concat(Tendencia(Coin.Symbol, "1s", 3), "/", Tendencia(Coin.Symbol, "1s", 5), "/", Tendencia(Coin.Symbol, "1s", 8), "/", Tendencia(Coin.Symbol, "1s", 10), "/", Tendencia(Coin.Symbol, "1s", 15))
 				Using cmd As New SQLiteCommand With {
 					.Connection = SQLiteConnection,
 					.CommandText = String.Concat("INSERT INTO tSells (Coin, Date, ProfitUSDTbr, Quantity, ProfitUSDT, SL, Comments, Tendencia) VALUES (""", Coin.Symbol, """,""", _Date, """,""", CStr(ProfitUSDTbr).Replace(",", "."), """,""", CStr(Quantity).Replace(",", "."), """,""", CStr(ProfitUSDT).Replace(",", "."), """,""", If(Coin.SL, "SI", "NO"), """,""", Comments, """,""", tend, """);")}
